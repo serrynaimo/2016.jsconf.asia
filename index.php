383d38ea28c974dcd0948c085b459f951fedf8cc
serrynaimo <serrynaimo@me.com>
2016-06-14 11:05:40 +0800

    updating tracking

--- index.php
+++ index.php
@@ -98,13 +98,13 @@
   <script src="./js/animation.js"></script>
 
   <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-31025490-9', 'auto');
-    ga('send', 'pageview');
+  ga('create', 'UA-31025490-12', 'auto');
+  ga('send', 'pageview');
 
 
    CountDownTimer('Fri, 22 Jun 2016 14:00:00 +0800', 'countdown');
