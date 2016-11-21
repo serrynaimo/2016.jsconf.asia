5102dd75d737f956b4ac87ad05184932826acc44
serrynaimo <serrynaimo@me.com>
2016-11-21 14:29:54 +0800

    draft lunch page

---
+++ lunch/index.php
@@ -0,0 +1,140 @@
+<?php
+  date_default_timezone_set('Asia/Singapore');
+?>
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:og="http://ogp.me/ns#"
+      xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+	<meta charset="UTF-8">
+	<title>CSSConf.Asia and JSConf.Asia Lunch Partners</title>
+	<link href='//fonts.googleapis.com/css?family=Doppio+One|Open+Sans:100' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="shortcut icon" href="favicon.png" >
+	<?php if(!strpos($_SERVER['HTTP_USER_AGENT'],'iPad')) { ?>
+	<meta name="viewport" content="width=500,max-width=500" />
+	<?php } ?>
+	<meta property="og:title" content="CSSConf.Asia and JSConf.Asia Lunch Partners"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:url" content="https://2016.jsconf.asia/lunch"/>
+	<meta property="og:image" content="https://2016.jsconf.asia/assets/og_jsconfasia.png"/>
+	<meta property="og:site_name" content="JSConf.Asia"/>
+	<meta property="og:description" content="CSSConf.Asia and JSConf.Asia Lunch Partners at the Capitol Piazza 2016"/>
+	<meta name="title" content="CSSConf.Asia and JSConf.Asia Lunch Partners"/>
+	<meta name="description" content="CSSConf.Asia and JSConf.Asia Lunch Partners at the Capitol Piazza 2016"/>
+</head>
+<body>
+ 
+	<nav>
+		<a href="#kh"><img src="kh.png"></a>
+		<a href="#nuri"><img src="nuri.png"></a>
+		<a href="#dazzling"><img src="dazzling.png"></a>
+		<a href="#equi"><img src="equilibrium.png"></a>
+		<a href="#ryo"><img src="ryo.png"></a>
+		<a href="#tap"><img src="tap.png"></a>
+		<a href="#nalan"><img src="nalan.png"></a>
+		<a href="#spizza"><img src="spizza.png"></a>
+	</nav>
+
+	<section>
+		
+		<h1><img src="lunchpartner.png" alt="CSSConf.Asia and JSConf.Asia Lunch Partners" border="0" /></h1>
+
+		<p>For CSSConf.Asia and JSConf.Asia<br/>24th - 26th November 2016</p>
+
+		<hr/>
+
+		<div>
+			<a name="kh"></a>
+
+			<p>Level 2</p>
+			<img src="kh.png" alt="Kaiserhaus" />
+
+			<h2>3 Course Lunch </h2>
+			<h3>STARTER</h3>
+			<p>Soup of the day served with olive baguette </p>
+
+			<h3>ROASTED CHICKEN </h3>
+			<p>Half a roasted organic chicken with Lecsó, a Hungarian ratatouille Served with salad and French fries </p>
+			or
+			<h3>MEAT PATTIES </h3>
+			<p>Grilled pork and beef patties with brown sauce Served with mash potatoes </p>
+			or
+			<h3>PESTO LINGUINE (V) </h3>
+			<p>Linguine with basil Pesto and flavored with spicy chili peppers</p>
+
+			<h3>DESSERT</h3>
+			<p>A slice of home-made cake or pastry from our pastry counter <br/>
+			Choice of Soft Drink Coke / Coke Light / Sprite / Home-made Ice Tea</p>
+
+			<span>40</span>
+
+		</div>
+
+		<hr/>
+
+		<div>
+			<a name="equi"></a>
+
+			<p>Ground Level</p>
+			<img src="equilibrium.png" alt="Equilibrium" />
+
+			<h2>Petto/Pasta</h2>
+
+			<h3>TRIO OF STARTER </h3>
+			<p>Starter Of Duck Rillettes, Antipasto Of The Day, Homemade Polenta Fritto With Spicy Garlic Aioli And Homemade Bread </p>
+
+			<h4>Choice of Main</h4>
+			<h3>PETTO DI POLLO ALLE ERBE AROMATICHE SOUS VIDED CHICEN BREAST </h3>
+			<p>Pan Seared Herb Butter Infused Chicken Breast With Chef’s Pick of Vegetables And Sauce</p>
+			<h3>PASTA ARRABIATA CON POLLO </h3>
+			<p>Pasta Simmered In A Homemade Spicy Garlic Infused Tomato Sauce With Chicken </p>
+			<h3>PASTA BOSCAIOLA </h3><p>Pasta Simmered In A Garlic And Herb Infused Tomato Sauce With Mushrooms And Onions </p>
+			<h3>PASTA CON PROSCUITTO IN AGLIO OLIO E PEPERONCINO </h3>
+			<p>Pasta Tossed In Hot Olive Oil, Copious Amounts Of Garlic, Splash Of Homemade Broth, Chilli And Parsley. Topped With Tomato Concasse, Rucola Leaves And Thinly Sliced Parma Ham </p>
+			<h3>PASTA AL FUNGHI CREMOSO </h3>
+			<p>Pasta Simmered In A Rich Creamy Sauce Infused With Farm Mushrooms And Shaved Parmesan  </p>
+
+			<h4>Beverage</h4>
+			<h3>CHOICE OF NON ALCOHOLIC DRINK</h3>
+
+			<span>30</span>
+
+			<h2>Pizza</h2>
+
+			<h3>TRIO OF STARTER </h3>
+			<p>Starter Of Duck Rillettes, Antipasto Of The Day, Homemade Polenta Fritto With Spicy Garlic Aioli And Homemade Bread </p>
+
+			<h4>Choice of Pizza</h4>
+			<h3>FUNGHI E UOVA </h3>
+			<p>Hand Opened Italian Thin Crust Pizza With Tomato Sauce, Mozzarella Cheese, Garlic, Farm Mushrooms And Egg </p>
+			<h3>VEGETARIANO </h3>
+			<p>Hand Opened Italian Thin Crust Pizza With Tomato Sauce, Mozzarella Cheese, Sliced Onions, Mixed Olives, Bell Peppers And Farm Mushrooms</p>
+			<h3>GAMMON E FUNGHI </h3>
+			<p>Hand Opened Italian Thin Crust Pizza With Tomato Sauce, Mozzarella Cheese, Oregano, Gammon Ham, Onions And Mushrooms </p>
+
+			<h4>Beverage</h4>
+			<h3>CHOICE OF NON ALCOHOLIC DRINK</h3>
+
+			<span>30</span>
+
+		<div>
+
+
+	</section>
+
+	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.3.js"></script>
+	<script type="text/javascript">window.jQuery || document.write('<script src="./js/jquery.min.js">\x3C/script>')</script>
+
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-31025490-12', 'auto');
+	ga('send', 'pageview');
+
+	</script>
+</body>
+</html>
